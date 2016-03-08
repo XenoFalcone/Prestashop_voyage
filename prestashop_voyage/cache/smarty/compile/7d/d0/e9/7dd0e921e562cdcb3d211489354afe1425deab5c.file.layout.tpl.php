857c15fe08ca646dc1bc14ff412b57aa2431c107
Yann Allart <Yann Allart>
2016-03-08 14:03:25 +0100

    BDD

---
+++ prestashop_voyage/cache/smarty/compile/7d/d0/e9/7dd0e921e562cdcb3d211489354afe1425deab5c.file.layout.tpl.php
@@ -0,0 +1,35 @@
+<?php /* Smarty version Smarty-3.1.19, created on 2016-03-08 13:58:15
+         compiled from "C:\wamp\www\Dm_prestashop\prestashop_voyage\admin003dzq488\themes\default\template\controllers\login\layout.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:753556decc67d512f2-76329339%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '7dd0e921e562cdcb3d211489354afe1425deab5c' => 
+    array (
+      0 => 'C:\\wamp\\www\\Dm_prestashop\\prestashop_voyage\\admin003dzq488\\themes\\default\\template\\controllers\\login\\layout.tpl',
+      1 => 1452095428,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '753556decc67d512f2-76329339',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'header' => 0,
+    'page' => 0,
+    'footer' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_56decc67da3b03_56640919',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_56decc67da3b03_56640919')) {function content_56decc67da3b03_56640919($_smarty_tpl) {?>
+
+<?php echo $_smarty_tpl->tpl_vars['header']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['page']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['footer']->value;?>
+<?php }} ?>
